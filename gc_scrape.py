49fa66fc81681ce1652f943b98c5e21f4ac1d33b
xoyito <codyschristopherson@gmail.com>
2021-11-23 14:48:11 -0700

    finished scrape of General Conference talks, added to parallel corpora eng.txt and kek.txt, working on allignment issue

---
+++ gc_scrape.py
@@ -0,0 +1,77 @@
+import requests_html
+import re
+import os
+# LOOK AT THIS LINK https://www.churchofjesuschrist.org/study/general-conference/2016/10/look-to-the-book-look-to-the-lord?lang=kek I don't think this is Q'eqchi'
+# links = ["https://www.churchofjesuschrist.org/study/general-conference/2021/04/11nelson?lang=eng", "https://www.churchofjesuschrist.org/study/general-conference/2021/04/11nelson?lang=kek"]
+links = ["https://www.churchofjesuschrist.org/study/general-conference/2021/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2021/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2020/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2020/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2019/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2019/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2018/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2018/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2017/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2016/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2016/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2015/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2015/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2014/04?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2007/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2006/10?lang=kek",
+        "https://www.churchofjesuschrist.org/study/general-conference/2003/04?lang=kek"]
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+session = requests_html.HTMLSession()
+
+talk_links = []
+for link in links:
+    print(f"Getting talk links from {link}")
+    r = session.get(link, headers=headers)
+    talks = r.html.find('body')
+    temp_links = [i.absolute_links for i in talks]
+    temp_links = sorted(list(temp_links[0]))
+    
+    for temp in temp_links:
+        talk_links.append(temp)
+
+talk_links = [i for i in talk_links if not re.search(r"session\?lang=kek", i)] #: Remove whole session pages without text
+talk_links = [i for i in talk_links if re.search(r"lang=kek$", i)] #: include only Q'eqchi' links in list
+
+with open("scraped_links.txt", "w", encoding="utf8") as link_file:
+    for link in talk_links:
+        link_file.write(link + "\n")
+
+print(f"Links collected for {str(len(talk_links))} LDS General Conference talks in Q'eqchi'!")
+
+for link in talk_links:
+    print(f"Getting text from {link}...")
+    r = session.get(link, headers=headers)
+    kek_text = r.html.find(".body-block")
+    kek_text = [i.text for i in kek_text]
+
+    try:
+        kek_text = kek_text[0]
+    except:
+        print("This link contains no text, continuing....")
+        continue
+    
+    english_link = re.sub(r"\?lang=kek$", "?lang=eng", link)
+
+    print(f"Getting English text from {english_link}...")
+    
+    r = session.get(english_link, headers=headers)
+    eng_text = r.html.find(".body-block")
+    eng_text = [i.text for i in eng_text]
+    eng_text = eng_text[0]
+
+    kek_filename = os.path.basename(link)
+    kek_filename = re.sub("\?lang=kek$", "-kek", kek_filename)
+
+    with open("KEK_TXT\\" + kek_filename + ".txt", "w", encoding="utf8") as outfile:
+        outfile.write(kek_text)
+
+    eng_filename = os.path.basename(english_link)
+    eng_filename = re.sub("\?lang=eng$", "-eng", eng_filename)
+    with open("ENG_TXT\\" + eng_filename + ".txt", "w", encoding="utf8") as outfile:
+        outfile.write(eng_text)
--- parallel.py
+++ parallel.py
@@ -7,12 +7,15 @@ kek_files= os.listdir("KEK_TXT")
 eng = ""
 kek = ""
 
-for i, file in enumerate(eng_files):
+for file in eng_files:
     with open("ENG_TXT\\" + file, "r", encoding="utf8") as eng_file:
         eng = eng + eng_file.read()
+    print(f"Loading {file}...")
     
-    with open("KEK_TXT\\" + kek_files[i], "r", encoding="utf8") as kek_file:
-        kek = kek + kek_file.read()
+    kek_file = re.sub(r"\-eng.txt$", "-kek.txt", file)
+    print(f"Loading {kek_file}...")
+    with open("KEK_TXT\\" + kek_file, "r", encoding="utf8") as queq_file:
+        kek = kek + queq_file.read()
 
 #####FIXME ISSUE WITH HOW SENTENCES LINE UP IN THE BIBLE SEE withbible.txt#####
 # # bible processing
@@ -35,16 +38,24 @@ eng = re.sub(r"\n", " ", eng)
 eng = re.sub(r"[0-9]", "", eng)
 eng = re.sub(r"“|”", "", eng)
 eng = re.sub(r"\t", "", eng)
+eng = re.sub(r"\.\.\.", "", eng)
+eng = re.sub(r"…", "", eng)
 
 kek = re.sub(r"\n", " ", kek)
 kek = re.sub(r"[0-9]", "", kek)
 kek = re.sub(r"“|”", "", kek)
 kek = re.sub(r"\t", "", kek)
+kek = re.sub(r"\.\.\.", "", kek)
+kek = re.sub(r"…", "", kek)
 
 eng_tokens = sent_tokenize(eng)
 kek_tokens = sent_tokenize(kek)
 
-with open("tuples.txt", "w", encoding="utf8") as corp_file:
-    for i, eng_sentence in enumerate(eng_tokens):
-        tuple = eng_sentence + "\t" + kek_tokens[i]
-        corp_file.write(tuple + "\n")
+with open("eng.txt", "w", encoding="utf8") as corp_file:
+        for eng_sentence in eng_tokens:
+            corp_file.write(eng_sentence + "\n")
+
+with open("kek.txt", "w", encoding="utf8") as kek_file:
+    for kek_sentence in kek_tokens:
+        kek_file.write(kek_sentence + "\n")
+        
